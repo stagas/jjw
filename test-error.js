3df78108a515e343cce564e49611efda55395861
George Stagas <gstagas@gmail.com>
2011-03-02 22:18:36 +0200

    You can throw from scrape functions now

--- test-error.js
+++ test-error.js
@@ -3,6 +3,11 @@ var jjw = require('./jjw')
 // error handling
 
 jjw('<base href="http://www.google.com"><div class=".so">not cool</div>', function($) { return $('.so').text() }, function(err, res) {
-  if (err) throw err
-  console.log(res)
+  if (err) console.log('ERROR:', err)
+  else console.log(res)
+})
+
+jjw('<div class=".so">not cool</div>', function($) { throw new Error('Some kind of error') }, function(err, res) {
+  if (err) console.log('ERROR:', err)
+  else console.log(res)
 })
--- worker.js
+++ worker.js
@@ -8,11 +8,15 @@ worker.onmessage = function(msg) {
   })
   jsdom.env(msg.body, [ __dirname + '/jquery.js' ], function(err, window) {
     if (err) return worker.postMessage({ error: err[0] })
-    var result
-    actions.forEach(function(action) {
-      result = action(window.jQuery)
-      if (result == null) result = '' // == null catches null and undefined
-      worker.postMessage(result)
-    })
+    try {
+      var result
+      actions.forEach(function(action) {
+        result = action(window.jQuery)
+        if (result == null) result = '' // == null catches null and undefined
+        worker.postMessage(result)
+      })
+    } catch(e) {
+      return worker.postMessage({ error: e })
+    }
   })
 }
